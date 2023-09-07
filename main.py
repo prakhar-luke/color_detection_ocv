53f614c77ee3aaf7565def1fd3a6b114a710d134
Prakhar <prakhar.luke@gmail.com>
2023-09-08 02:55:04 +0530

    updated util

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import cv2
 from PIL import Image
 from util import get_limits
 
-yellow = [0,255,255] # color in BGR
+green = [0,255,0] # color in BGR
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
     
-    l_lim, u_lim = get_limits(color = yellow)
+    l_lim, u_lim = get_limits(color = green)
 
     mask = cv2.inRange(hsv_img, l_lim, u_lim)
     
     mask_ = Image.fromarray(mask) # convert numpy array to pilow
     bbox = mask_.getbbox()
+
     if bbox is not None:
         x1, y1, x2, y2 = bbox
-        cv2.rectangle(frame, (x1,y1),(x2,y2), (0,255,0), 5)
+        frame = cv2.rectangle(frame, (x1,y1),(x2,y2), (0,255,0), 5)
 
     cv2.imshow('frame', frame)
 
