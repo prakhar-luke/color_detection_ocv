8c639fdedc9cbb8ad4db75d415e18208c5a62a62
Prakhar <prakhar.luke@gmail.com>
2023-09-07 19:19:37 +0530

    yellow color detection test passed try 1

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from PIL import Image
+from util import get_limits
+
+yellow = [0,255,255] # color in BGR
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+    
+    l_lim, u_lim = get_limits(color = yellow)
+
+    mask = cv2.inRange(hsv_img, l_lim, u_lim)
+    
+    mask_ = Image.fromarray(mask) # convert numpy array to pilow
+    bbox = mask_.getbbox()
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(frame, (x1,y1),(x2,y2), (0,255,0), 5)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
--- util.py
+++ util.py
@@ -2,4 +2,13 @@ import numpy as np
 import cv2
 
 def get_limits(color):
-    pass
+    c = np.uint8([[color]])
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    l_lim = hsvC[0][0][0] - 10,100,100
+    u_lim = hsvC[0][0][0] - 10,255,255
+    
+    l_lim = np.array(l_lim, dtype=np.uint8)
+    u_lim = np.array(u_lim, dtype=np.uint8)
+
+    return l_lim, u_lim
